de6696b6ff307b7f5b7d17fdf915f924f050df39
Anand.Narayan <arnarayan@gmail.com>
2022-04-19 20:58:03 -0600

    - Create Project structure - Create View Model

--- Module5Lesson2/Module5Lesson2App.swift
+++ Module5Lesson2/Module5Lesson2App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Module5Lesson2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView().environmentObject(ContentModel())
         }
     }
 }
---
+++ Module5Lesson2/ViewModels/ContentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentViewModel.swift
+//  Module5Lesson2
+//
+//  Created by Anand Narayan on 2022-04-19.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    // @Published var 
+}
