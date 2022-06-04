7d34da5c0c3040de2ce05efda4988111ab96d3a3
Anand.Narayan <arnarayan@gmail.com>
2022-06-04 16:48:50 -0600

    Showing a dynamic header in the results

--- Module5Lesson2/Views/TestResultView.swift
+++ Module5Lesson2/Views/TestResultView.swift
@@ -11,9 +11,16 @@ struct TestResultView: View {
     var numResults: Int
     @EnvironmentObject var model: ContentModel
     
+    var headerResult: String {
+        if (numResults<5) {
+            return "Keep Trying"
+        }
+        return "Doing Awesome!"
+    }
+    
     var body: some View {
         VStack {
-            Text("Doing Great!").font(.title)
+            Text("\(headerResult)").font(.title)
             Text("You got \(numResults) out of \(model.selectedModule?.test.questions.count ?? 0) questions correct.")
             Button(action: {
                 model.selectedTag = nil
